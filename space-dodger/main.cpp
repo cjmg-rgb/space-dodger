d1bab19ed7988c7d37c6cade8ec8d29cca54a4b8
Cjade <garciachristianjade0@gmail.com>
2023-12-06 01:03:48 +0800

    Created a player
    -Added a spritesheet
    -Added player movement

--- space-dodger/main.cpp
+++ space-dodger/main.cpp
@@ -1,23 +1,67 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    //------------------------------- INITIALIZE -----------------------------------
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "My Window");
 
-    while (window.isOpen())
-    {
+
+    //------------------------------- INITIALIZE -----------------------------------
+
+    //------------------------------- LOAD -----------------------------------------
+
+    int xIndex = 0;
+    int yIndex = 0;
+
+    sf::Texture playerTexture;
+    sf::Sprite playerSprite;
+    playerTexture.loadFromFile("assets/textures/player/spritesheet.png");
+    playerSprite.setTexture(playerTexture);
+    playerSprite.setTextureRect(sf::IntRect(32*xIndex, 32*yIndex, 32, 32));
+    playerSprite.scale(3, 3);
+    playerSprite.setRotation(180);
+    playerSprite.setPosition(sf::Vector2f(400, 550));
+
+    //------------------------------- LOAD ------------------------------------------
+ 
+    while (window.isOpen()) {
+
+
+        //------------------------------- Event Loop -----------------------------------
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) 
                 window.close();
         }
+        sf::Vector2f playerPos = playerSprite.getPosition();
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+            playerSprite.setPosition(playerPos + sf::Vector2f(1, 0));
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+            playerSprite.setPosition(playerPos + sf::Vector2f(-1, 0));
+
+        //------------------------------- Event Loop -----------------------------------
+
+        
+
+        
+        //------------------------------- UPDATE -----------------------------------
 
         window.clear();
-        window.draw(shape);
+
+        window.draw(playerSprite);
+
         window.display();
+        window.setFramerateLimit(60);
+
+        //------------------------------- UPDATE -----------------------------------
+
     }
 
     return 0;
