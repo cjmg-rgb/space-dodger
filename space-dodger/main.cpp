68e9b9adede36588315e0019e4c0b632a3909f1e
Cjade <garciachristianjade0@gmail.com>
2023-12-06 07:57:14 +0800

    Enemy logic
    -Added enemy spawning logic
    -Spawns and moves enemy

--- space-dodger/main.cpp
+++ space-dodger/main.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <ctime>
+#include <vector>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -9,12 +11,15 @@ int main()
 {
     //------------------------------- INITIALIZE -----------------------------------
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "My Window");
+    srand(time(NULL));
 
 
     //------------------------------- INITIALIZE -----------------------------------
 
     //------------------------------- LOAD -----------------------------------------
 
+
+    // Player
     int xIndex = 0;
     int yIndex = 0;
 
@@ -23,9 +28,26 @@ int main()
     playerTexture.loadFromFile("assets/textures/player/spritesheet.png");
     playerSprite.setTexture(playerTexture);
     playerSprite.setTextureRect(sf::IntRect(32*xIndex, 32*yIndex, 32, 32));
-    playerSprite.scale(3, 3);
+    playerSprite.scale(2, 2);
     playerSprite.setRotation(180);
     playerSprite.setPosition(sf::Vector2f(400, 550));
+
+    // Enemy
+    int enemyList[5][2] = {
+        {3, 0}, {6, 0}, {9, 0},
+        {0, 4}, {6, 4}
+    };
+    int enemyIndex = rand() % 4;
+    int enemySpeed = (rand() % 5) + 1;
+    int enemy[2] = { enemyList[enemyIndex][0], enemyList[enemyIndex][1] };
+
+    sf::Texture enemyTexture;
+    sf::Sprite enemySprite;
+    enemyTexture.loadFromFile("assets/textures/enemy/spritesheet.png");
+    enemySprite.setTexture(enemyTexture);
+    enemySprite.setTextureRect(sf::IntRect(32*enemy[0], 32*enemy[1], 32, 32));
+    enemySprite.scale(2, 2);
+    enemySprite.setPosition(sf::Vector2f(rand() % WIDTH-64, 0));
 
     //------------------------------- LOAD ------------------------------------------
  
@@ -48,14 +70,28 @@ int main()
 
         //------------------------------- Event Loop -----------------------------------
 
-        
+        //------------------------------- AI movement ----------------------------------
+        sf::Vector2f enemyPos = enemySprite.getPosition();
+        enemySprite.setPosition(enemyPos + sf::Vector2f(0, enemySpeed));
 
-        
+        if (enemyPos.y >= HEIGHT) {
+            enemyIndex = rand() % 4;
+            enemySpeed = (rand() % 5) + 1;
+            enemy[0] = enemyList[enemyIndex][0];
+            enemy[1] = enemyList[enemyIndex][1];
+            enemySprite.setTextureRect(sf::IntRect(32 * enemy[0], 32 * enemy[1], 32, 32));
+            enemySprite.setPosition(sf::Vector2f(rand() % WIDTH - 64, 0));
+        }
+
+        //------------------------------- AI movement ----------------------------------
+
+
         //------------------------------- UPDATE -----------------------------------
 
         window.clear();
 
         window.draw(playerSprite);
+        window.draw(enemySprite);
 
         window.display();
         window.setFramerateLimit(60);
